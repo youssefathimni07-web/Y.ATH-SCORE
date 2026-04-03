1b8b2c3f2c8f86ae454549338f3c47bbf1be2280
youssefathimni07-web <youssefathimni07@gmail.com>
2026-04-03 15:50:41 +0100

    Create Navbar.tsx

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div style={{
+      padding: 15,
+      background: "#020617",
+      fontSize: 20,
+      fontWeight: "bold"
+    }}>
+      ⚽ Football App
+    </div>
+  );
+}
