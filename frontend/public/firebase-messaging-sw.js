a7b1b9323c0e1f8c000f28731ad65268383068ba
youssefathimni07-web <youssefathimni07@gmail.com>
2026-04-03 15:42:27 +0100

    Create firebase-messaging-sw.js

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "XXX",
+  projectId: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  });
+});
